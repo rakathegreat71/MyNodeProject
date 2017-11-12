e6617162101f0efb4581d8129d5a16e14ce7e7ec
rakesh <rakathegreat71@gmail.com>
2017-11-12 23:45:21 +0530

    body parser is working

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 var express = require("express");
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var nodemailer  = require('nodemailer');
+
+
 var app = express();
+
+// body parser will get the body data and converts in into a json object
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 // get port number
 var port = process.env.PORT || 9000;
@@ -10,6 +19,12 @@ app.use(express.static(__dirname));
 app.get("/", function (req, res) {
 	res.reder("index");
 });
+
+app.post('/send', function(req, res){
+	// res.send("this is working" + req.body);
+	console.log(req.body);
+	res.send("your mail is sent Mr." + req.body.name);
+})
 
 app.listen(port, function(){
 	console.log("app running");
